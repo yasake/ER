79a2eddcb7f43898bc611abbbade9d751bc0e9a9
Mohammad Diyab Alyasaqi <mohammad.alyasaqi@mni.thm.de>
2019-10-19 13:32:11 +0200

    first commit

---
+++ 5 Runaway (Merge of Project)/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,118 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public bool win = false;
+    public float speed = 6f;            // The speed that the player will move at.
+
+    Vector3 movement;                   // The vector to store the direction of the player's movement.
+    Animator anim;                      // Reference to the animator component.
+    Rigidbody playerRigidbody;          // Reference to the player's rigidbody.
+    int floorMask;                      // A layer mask so that a ray can be cast just at gameobjects on the floor layer.
+    float camRayLength = 100f;          // The length of the ray from the camera into the scene.
+	public AudioClip CoinSound = null;
+	private AudioSource mAudioSource = null;
+
+
+	void Start()
+	{
+			 InvokeRepeating("resetSpeed", 5f, 3f); 	
+	}
+    void Awake ()
+    {
+        // Create a layer mask for the floor layer.
+        floorMask = LayerMask.GetMask ("Floor");
+
+        // Set up references.
+        anim = GetComponent <Animator> ();
+        playerRigidbody = GetComponent <Rigidbody> ();
+		mAudioSource = GetComponent<AudioSource> ();
+    }
+
+
+    void FixedUpdate ()
+    {
+		
+		
+        // Store the input axes.
+        float h = Input.GetAxisRaw ("Horizontal");
+        float v = Input.GetAxisRaw ("Vertical");
+
+        // Move the player around the scene.
+        Move (h, v);
+
+        // Turn the player to face the mouse cursor.
+        Turning ();
+
+        // Animate the player.
+        Animating (h, v);
+    }
+
+    void Move (float h, float v)
+    {
+        // Set the movement vector based on the axis input.
+        movement.Set (h, 0f, v);
+        
+        // Normalise the movement vector and make it proportional to the speed per second.
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        // Move the player to it's current position plus the movement.
+        playerRigidbody.MovePosition (transform.position + movement);
+    }
+
+    void Turning ()
+    {
+        // Create a ray from the mouse cursor on screen in the direction of the camera.
+        Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+        // Create a RaycastHit variable to store information about what was hit by the ray.
+        RaycastHit floorHit;
+
+        // Perform the raycast and if it hits something on the floor layer...
+        if(Physics.Raycast (camRay, out floorHit, camRayLength, floorMask))
+        {
+            // Create a vector from the player to the point on the floor the raycast from the mouse hit.
+            Vector3 playerToMouse = floorHit.point - transform.position;
+
+            // Ensure the vector is entirely along the floor plane.
+            playerToMouse.y = 0f;
+
+            // Create a quaternion (rotation) based on looking down the vector from the player to the mouse.
+            Quaternion newRotation = Quaternion.LookRotation (playerToMouse);
+
+            // Set the player's rotation to this new rotation.
+            playerRigidbody.MoveRotation (newRotation);
+        }
+    }
+
+    void Animating (float h, float v)
+    {
+        // Create a boolean that is true if either of the input axes is non-zero.
+        bool walking = h != 0f || v != 0f;
+
+        // Tell the animator whether or not the player is walking.
+        anim.SetBool ("IsWalking", walking);
+    }
+	
+		void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag.Equals ("Coin")) {
+			if(mAudioSource != null && CoinSound != null){
+				mAudioSource.PlayOneShot(CoinSound);
+			}
+			speed  = 12f;
+			Destroy(other.gameObject);
+		}
+		else if (other.gameObject.tag.Equals ("Finish")) {
+			win = true;
+			
+		}
+		
+	}
+	
+	void resetSpeed ()
+     {
+		if(speed != 6f){
+			speed = 6f;
+		}
+	 }
+}
---
+++ 5 Runaway (Merge of Project)/Assets/Scripts/YouWin.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class YouWin : MonoBehaviour
+{
+    public PlayerMovement playerMovement;       // Reference to the player's health.
+    public float restartDelay = 5f;         // Time to wait before restarting the level
+	public Text text;
+	public EnemyHealth enemyHealth;
+
+
+    Animator anim;                          // Reference to the animator component.
+    float restartTimer;                     // Timer to count up to restarting the level
+
+
+    void Awake ()
+    {
+        // Set up the reference.
+        anim = GetComponent <Animator> ();
+    }
+
+
+    void Update ()
+    {
+        // If the player has run out of health...
+        if(playerMovement.win == true)
+        {
+			enemyHealth.currentHealth = 0;
+            // ... tell the animator the game is over.
+            anim.SetTrigger ("GameWin");
+			text.text = "You Win!";
+			
+            // .. increment a timer to count up to restarting.
+            restartTimer += Time.deltaTime;
+
+            // .. if it reaches the restart delay...
+            if(restartTimer >= restartDelay)
+            {
+                // .. then reload the currently loaded level.
+                Application.LoadLevel(Application.loadedLevel);
+            }
+        }
+    }
+}
